51be073ac0bca88400a0b1756fabe3ad0c16eefa
Adaptor <56938414+Akitoshi-Kamil@users.noreply.github.com>
2020-12-22 12:56:13 +0100

    Class TicTacToeGameBoard and Tests Added

---
+++ app/src/test/java/com/example/tictactoeandroid/TicTacToeGameBoardTest.java
@@ -0,0 +1,32 @@
+package com.example.tictactoeandroid;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+@RunWith(JUnit4.class)
+public class TicTacToeGameBoardTest {
+    TicTacToeGameBoard gameBoard;
+
+    @Before
+    public void init(){
+        gameBoard = new TicTacToeGameBoard();
+    }
+    @Test
+    public void testSetCircleOnEmptyFieldAndTryOverwrite(){
+        assertTrue(gameBoard.setField(0, 0, Field.Circle));
+        assertFalse(gameBoard.setField(0, 0, Field.Cross));
+    }
+    @Test
+    public void testGetGameBoard(){
+        Field[][] board = new Field[3][3];
+        for (Field[] e: board)
+            Arrays.fill(e, Field.Empty);
+
+        assertEquals(gameBoard.getGameBoard(), board);
+    }
+}
