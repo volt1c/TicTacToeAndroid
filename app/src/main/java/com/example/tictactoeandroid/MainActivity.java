7472bac7be71535ed297ec7651646f39630612b1
BuildTools <unconfigured@null.spigotmc.org>
2020-12-23 17:38:15 +0100

    Add Exit button.Change layout.Add Exit function

--- app/src/main/java/com/example/tictactoeandroid/MainActivity.java
+++ app/src/main/java/com/example/tictactoeandroid/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tictactoeandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void exitGame(View view){
+        finish();
+        System.exit(0);
     }
 }
