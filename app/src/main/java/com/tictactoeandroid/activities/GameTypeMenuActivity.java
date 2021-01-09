de10509fe546f7a3d57f960f6483cc2b11970b9d
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-01-09 01:21:52 +0100

    2 new Empty Activities (TODO)

---
+++ app/src/main/java/com/tictactoeandroid/activities/GameTypeMenuActivity.java
@@ -0,0 +1,16 @@
+package com.tictactoeandroid.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tictactoeandroid.R;
+
+public class GameTypeMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_type_menu);
+    }
+}
