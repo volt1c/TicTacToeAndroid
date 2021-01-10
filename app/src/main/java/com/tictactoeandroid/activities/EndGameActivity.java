6076259c9a00ca12d2227391b1b6b9fea38ff6d6
Dawid Czyżewski <dawidek12.04@wp.pl>
2021-01-10 20:43:45 +0100

    update
    Signed-off-by: Dawid Czyżewski <dawidek12.04@wp.pl>

--- app/src/main/java/com/tictactoeandroid/activities/EndGameActivity.java
+++ app/src/main/java/com/tictactoeandroid/activities/EndGameActivity.java
@@ -2,6 +2,8 @@ package com.tictactoeandroid.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.tictactoeandroid.R;
@@ -12,5 +14,8 @@ public class EndGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_game);
+
+        SharedPreferences sp = getApplicationContext().getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+        int result = sp.getInt("gameResult",0);
     }
 }
