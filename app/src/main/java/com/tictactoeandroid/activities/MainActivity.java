a89f6e44b1f9a528397c7a8abdbe4bc15024ca8e
Dawid Czyżewski <dawidek12.04@wp.pl>
2021-01-10 20:48:25 +0100

    update
    Signed-off-by: Dawid Czyżewski <dawidek12.04@wp.pl>

--- app/src/main/java/com/tictactoeandroid/activities/MainActivity.java
+++ app/src/main/java/com/tictactoeandroid/activities/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startGame(View view){
-        Intent intent = new Intent(MainActivity.this, GameActivity.class);
+        Intent intent = new Intent(MainActivity.this, GameTypeMenuActivity.class);
         startActivity(intent);
         finish();
     }
