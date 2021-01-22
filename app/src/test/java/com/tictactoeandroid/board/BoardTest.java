55c3cab10ff43ed9607286c789aad41552e84d51
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-01-22 21:14:57 +0100

    refactoring

--- app/src/test/java/com/tictactoeandroid/board/BoardTest.java
+++ app/src/test/java/com/tictactoeandroid/board/BoardTest.java
@@ -26,46 +26,29 @@ public class BoardTest {
         assertEquals(board.getFields(), correct);
     }
     @Test
-    public void testGetSizeByChar(){
-        assertEquals(board.size('w'),3); // W-idth
-        assertEquals(board.size('h'),4); // H-eight
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testGetSizeByIllegalArgument(){
-        board.size('x');
-    }
-
-    @Test
-    public void testClone(){
+    public void testCloneBoard(){
         Board clone = board.clone();
 
         assertEquals(board.getFields(), clone.getFields());
     }
-
     @Test
     public void testCheckIsEmptyWhenEmpty(){
         assertTrue(board.isEmpty());
     }
-
     @Test
     public void testIsEmptyWhenNotEmpty(){
         board.setField(0, 0,1);
 
         assertFalse(board.isEmpty());
     }
-
     @Test
-    public void testCheckIsEmptyWorkOnEmptyField(){
-        assertTrue(board.isFieldEmpty(0,0));
+    public void testCheckIsFieldEmptyWhenFieldEmpty(){
         assertTrue(board.isFieldEmpty(2,3));
     }
-
     @Test
     public void testCheckIsEmptyBoardFull(){
         assertFalse(board.isFull());
     }
-
     @Test
     public void testCheckIsFullWhenBoardFull(){
         board.fill(2);
