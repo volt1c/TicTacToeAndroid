2dcc82179a87b51809a1cbc58c9cd3f1469f6063
Kamil Pawlaczyk <56938414+Agil-Dev@users.noreply.github.com>
2021-02-17 14:25:42 +0100

    refactor: removing unnecessary imports

--- app/src/main/java/com/tictactoeandroid/activities/listener/FieldOnClickListenerBuilder.java
+++ app/src/main/java/com/tictactoeandroid/activities/listener/FieldOnClickListenerBuilder.java
@@ -3,7 +3,6 @@ package com.tictactoeandroid.activities.listener;
 import android.view.View;
 
 import com.tictactoeandroid.activities.GameActivity;
-import com.tictactoeandroid.game.TicTacToeGame;
 
 public class FieldOnClickListenerBuilder {
     public View.OnClickListener build(int x, int y, GameActivity game) {
